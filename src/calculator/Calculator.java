5c7c2b6bf71c396babace49a72e079b8d8a5fb89
Saqib Ansari <saqans@gmail.com>
2013-11-11 18:43:01 -0500

    Calculator first commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author 1
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
